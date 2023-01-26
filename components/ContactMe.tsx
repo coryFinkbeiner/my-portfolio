e461ba4d367b07336e268e1e5af2296d7f178789
steeper <portland.steeper@gmail.com>
2023-01-26 02:45:24 -0500

    2:14:00, before the forms

---
+++ components/ContactMe.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+
+type Props = {}
+
+function ContactMe({}: Props) {
+  return (
+    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        Contact
+      </h3>
+
+      <div className='flex flex-col space-y-10'>
+        <h4 className='text-4xl font-semibold text-center'>
+          I have got just what you need.{' '}
+          <span className='decoration-[#F7AB0A]/50 underline'>Lets Talk</span>
+        </h4>
+
+        <div>
+          <div className='flex items-center space-x-5 justify-center'>
+            <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+            <p>616-340-5176</p>
+          </div>
+        </div>
+
+
+        <div>
+          <div className='flex items-center space-x-5 justify-center'>
+            <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+            <p>portland.steeper@gmail.com</p>
+          </div>
+        </div>
+
+
+        <div>
+          <div className='flex items-center space-x-5 justify-center'>
+            <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
+            <p>Grand Rapids, Michigan</p>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default ContactMe
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,14 +6,27 @@ type Props = {}
 function Projects({}: Props) {
   const projects= [1,2,3,4,5]
   return (
-    <div className='h-screen relative overflow-hidden flex-col text-left md:flex-row max-w-full justify evenly mx-auto items-center z-0'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1.5 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once:true }}
+      className='h-screen relative overflow-hidden flex-col text-left md:flex-row max-w-full justify evenly mx-auto items-center z-0'
+    >
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Projects
       </h3>
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects.map((project, i) => (
           <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-            <img
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once:true }}
               src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Spotify_App_Logo.svg/600px-Spotify_App_Logo.svg.png?20210620075506'
               alt=''
             />
@@ -40,7 +53,7 @@ function Projects({}: Props) {
       </div>
 
       <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12' />
-    </div>
+    </motion.div>
   )
 }
 
