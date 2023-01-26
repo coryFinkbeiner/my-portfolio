1072c022096322ee3575e1628cfb5e79cee3586c
steeper <portland.steeper@gmail.com>
2023-01-25 22:34:02 -0500

    up to speed through 58:00. about to add animations.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import Head from 'next/head';
 import type { NextPage } from 'next';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>My Portfolio</title>
       </Head>
 
       <Header />
 
-      {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About */}
 
