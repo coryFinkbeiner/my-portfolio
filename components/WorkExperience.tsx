cea4b390d756892063b6d642fa11d9993cba47f7
steeper <portland.steeper@gmail.com>
2023-01-26 19:26:08 -0500

    done with following the client part of the video, moving on to sanity. going to cut a client branch in case i want to go with a different back end.

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -16,7 +16,7 @@ function WorkExperience({}: Props) {
         Experience
       </h3>
 
-      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thum-[#F7AB0A]/80'>
+      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
