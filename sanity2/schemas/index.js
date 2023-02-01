15f6a1e1ec1d6f7e11e73bc91104be27724bebb4
steeper <portland.steeper@gmail.com>
2023-02-01 00:26:46 -0500

    seeing my login provider, successssssss....

---
+++ sanity2/schemas/index.js
@@ -0,0 +1,11 @@
+import pageInfo from './pageInfo';
+import experience from './experience';
+import skill from './skill';
+import social from './social';
+import project from './project';
+
+export const schemaTypes = [skill,
+  pageInfo,
+  experience,
+  social,
+  project]
--- sanity2/schemas/skill.js
+++ sanity2/schemas/skill.js
@@ -1,52 +1,28 @@
 export default {
-  name: 'experience',
-  title: 'Experience',
+  name: 'skill',
+  title: 'Skill',
   type: 'document',
   fields: [
     {
-      name: 'jobTitle',
-      title: 'JobTitle',
+      name: 'title',
+      title: 'Title',
+      description: 'Title of skill',
       type: 'string',
     },
     {
-      name: 'companyImage',
-      title: 'Company Image',
+      name: 'progress',
+      title: 'Progress',
+      type: 'number',
+      description: 'Progress of skill from 0 to 100%',
+      validation: (Rule) => Rule.min(0).max(100),
+    },
+    {
+      name: 'image',
+      title: 'Image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    },
-    {
-      name: 'company',
-      title: 'Company',
-      type: 'text',
-    },
-    {
-      name: 'dateStarted',
-      title: 'DateStarted',
-      type: 'date',
-    },
-    {
-      name: 'dateEnded',
-      title: 'DateEnded',
-      type: 'date',
-    },
-    {
-      name: 'isCurrentlyWorkingHere',
-      title: 'IsCurrentlyWorkingHere',
-      type: 'boolean',
-    },
-    {
-      name: 'technologies',
-      title: 'Technologies',
-      type: 'array',
-      of: [{ type: 'reference', to: { type: 'skill' } }],
-    },
-    {
-      name: 'points',
-      title: 'Points',
-      type: 'array',
-      of: [{ type: 'string' }],
     },
   ],
 }
